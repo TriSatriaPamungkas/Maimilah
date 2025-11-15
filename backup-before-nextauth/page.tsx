2f36b8e766f2738655626267869d751366c26399
Satria-kun21 <207948981+TriSatriaPamungkas@users.noreply.github.com>
2025-11-15 08:57:37 +0800

    Remove backup folder from repository

--- backup-before-nextauth/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/src/components/molecules/loginForm";
-
-export default function AdminLoginPage() {
-  return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-50">
-      <LoginForm />
-    </main>
-  );
-}
