de5339709e0af72eba9a89f530e88efad6ad20a1
Satria-kun21 <207948981+TriSatriaPamungkas@users.noreply.github.com>
2025-11-18 23:57:30 +0800

    route update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -142,7 +142,7 @@ export const authOptions: NextAuthOptions = {
 
   secret: process.env.NEXTAUTH_SECRET,
 
-  debug: process.env.NODE_ENV === "development",
+  debug: process.env.NODE_ENV === "production",
 };
 
 const handler = NextAuth(authOptions);
